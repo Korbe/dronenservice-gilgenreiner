9c94273b87a04884073e3765a6b716c6a36ad934
Lukas Korbitsch <korbitschl@gmail.com>
2026-04-24 12:53:02 +0200

    added powershell script for deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
-Write-Host "🚀 Building Vite app..." -ForegroundColor Cyan
+Write-Host "Build läuft..." -ForegroundColor Cyan
 npm run build
 
-Write-Host "📦 Uploading files to server..." -ForegroundColor Yellow
+Write-Host "Upload via SCP..." -ForegroundColor Yellow
 
 scp -r dist/* deployer@46.101.242.71:/var/www/gilgenreiner.at/current/
 
-Write-Host "✅ Deploy erfolgreich!" -ForegroundColor Green
+Write-Host "Deploy erfolgreich!" -ForegroundColor Green
