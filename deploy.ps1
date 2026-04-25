f2d0d8fb482993869cef5cca6f594961dd83eb11
Lukas Korbitsch <korbitschl@gmail.com>
2026-04-25 18:47:34 +0200

    script now deletes existing files

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,10 @@
 Write-Host "Build läuft..." -ForegroundColor Cyan
 npm run build
 
+Write-Host "Server wird bereinigt..." -ForegroundColor Yellow
+
+ssh deployer@46.101.242.71 "rm -rf /var/www/gilgenreiner.at/current/*"
+
 Write-Host "Upload via SCP..." -ForegroundColor Yellow
 
 scp -r dist/* deployer@46.101.242.71:/var/www/gilgenreiner.at/current/
